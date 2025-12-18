31e587597c723703b858f966f48c66eea89e7177
toluooshy <56281958+toluooshy@users.noreply.github.com>
2025-12-17 22:00:07 -0500

    Fixing bluesky feed manager bugs

--- bluesky-feed-manager/server/config.py
+++ bluesky-feed-manager/server/config.py
@@ -12,7 +12,7 @@ if not HOSTNAME:
     raise RuntimeError('You must set "HOSTNAME" in your .env file.')
 
 # DID for this service
-SERVICE_DID = f"did:web:{HOSTNAME}"
+config.SERVICE_DID = "did:web:feeds.princetonhci.social"
 
 # Optional global flags
 def _get_bool_env_var(value: str) -> bool:

25f89bc97d7ea2c60cbfef36d0529c3e1eafdd13
toluooshy <56281958+toluooshy@users.noreply.github.com>
2025-12-18 00:10:29 -0500

    Fixing bluesky feed manager bugs

--- bluesky-feed-manager/server/algos/feed.py
+++ bluesky-feed-manager/server/algos/feed.py
@@ -10,9 +10,9 @@ from server.models import Feed, FeedSource, FeedCache
 from collections import defaultdict
 import random
 
-CACHE_TTL = 30  # seconds
-RESPONSE_LIMIT = 100 # number of posts to be received from api response
-FEED_LIMIT = 1000 # number of total posts in a feed
+CACHE_TTL = 10  # seconds
+RESPONSE_LIMIT = 40 # number of posts to be received from api response
+FEED_LIMIT = 100 # number of total posts in a feed
 MAX_PER_AUTHOR = 5 # max posts per author in a feed
 
 CUSTOM_API_URL = os.environ.get("CUSTOM_API_URL")

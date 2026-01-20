db55477bf1cc50bf4980fa19d62b495f4b62e7b9
toluooshy <56281958+toluooshy@users.noreply.github.com>
2026-01-20 01:26:48 -0500

    Fixing feed manager issues

--- bluesky-feed-manager/server/algos/feed.py
+++ bluesky-feed-manager/server/algos/feed.py
@@ -11,7 +11,7 @@ from server.models import Feed, FeedSource, FeedCache, SearchCache
 from collections import defaultdict
 import random
 
-SEARCH_CACHE_TTL = 300  # 5 minutes for search results
+SEARCH_CACHE_TTL = 120  # 2 minutes for search results
 RESPONSE_LIMIT = 10 # number of posts to be received from api response
 FEED_LIMIT = 100 # number of total posts in a feed
 MAX_PER_AUTHOR = 10 # max posts per author in a feed
